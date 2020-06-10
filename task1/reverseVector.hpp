760b7fdcc7d758305465334e3cdaf29f49411327
Nessy90 <nessy0991@gmail.com>
2020-06-10 23:39:56 +0200

    task1

---
+++ task1/reverseVector.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <vector>
+
+std::vector<int> vectorToReverse{1, 2, 3, 4, 5};
+void print(std::vector<int>&);
+std::vector<int> reverseSolutionOne(std::vector<int>);
+std::vector<int> reverseSolutionTwo(std::vector<int>);
+std::vector<int> reverseSolutionThree(std::vector<int>);
+std::vector<int> reverseSolutionFour(std::vector<int>);
+std::vector<int> reverseSolutionFive(std::vector<int>);
+std::vector<int> reverseSolutionSix(std::vector<int>);
