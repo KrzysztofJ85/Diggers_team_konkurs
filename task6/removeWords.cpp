f0bab7ebc0a19f17e58c1dc509443e1068101201
lksobczyk <lksobczyk@wp.pl>
2020-06-11 13:27:35 +0200

    add count_if and refactoring

--- task6/removeWords.cpp
+++ task6/removeWords.cpp
@@ -1,27 +1,31 @@
-#include <string>
-#include <vector>
-#include <sstream>
-#include <iterator>
 #include <algorithm>
-#include <numeric>
-
+#include <cctype>
 #include <iostream>
+#include <iterator>
+#include <numeric>
+#include <string>
+#include <sstream>
+#include <vector>
 
-#define istream std::istream_iterator<std::string>
+using istream = std::istream_iterator<std::string>;
 
-std::string removeWords(const std::string& text){
+std::string removeWords(const std::string& text) {
     std::istringstream stext(text);
-    std::vector<std::string> words(istream{stext}, istream());
+    std::vector<std::string> words(istream{ stext }, istream());
 
-    words.erase(std::remove_if(words.begin(), words.end(), 
-        [](const std::string& word){
-            return word.size() == 4 || word.size() == 6;
-        }), words.end());
-    
+    words.erase(std::remove_if(words.begin(), words.end(),
+        [](const std::string& word) {
+        size_t letters = std::count_if(word.begin(), word.end(),
+            [](unsigned char c) { return std::isalpha(c); });
+        return letters == 4 || letters == 6;
+    }), words.end());
+
     return std::accumulate(std::next(words.begin()), words.end(), words[0],
-        [](std::string lhs, std::string rhs) {return std::move(lhs) + ' ' + rhs;}); 
+        [](std::string lhs, std::string rhs) {return std::move(lhs) + ' ' + rhs; });
 }
 
-int main(){
-    std::cout << removeWords("Ala ma koa.");
+int main() {
+    std::cout << removeWords("Ala ma kota a kot ma Ale, jednak to bylo dawno temu.");
+
+    return 0;
 }
