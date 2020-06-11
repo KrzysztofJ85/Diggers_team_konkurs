9e4a67072950d508c442446e704528800320a502
KrzysztofJ85 <krzysztof.jucha@gmail.com>
2020-06-11 11:53:48 +0200

    task3 v3 KrzysztofJ85

--- task3/task3.cpp
+++ task3/task3.cpp
@@ -2,7 +2,6 @@
 
 #include <algorithm>
 #include <iostream>
-#include <vector>
 
 const std::string palindrom = "ala a ala";
 const std::string not_palindrom = "ala ma kota";
@@ -42,8 +41,8 @@ void isPalindrome3(const std::string& str) {
 }
 
 void isPalindrome4(const std::string& str) {
-    std::string strcopy = str;
     std::cout << "isPalindrome4:\n";
+    std::string strcopy = str;
     for (auto it1 = strcopy.begin(), it2 = std::prev(strcopy.end()); it1 != strcopy.end(); ++it1, --it2) {
         if ((it1 - strcopy.begin()) < str.size() / 2) {
             std::swap(*it1, *it2);
@@ -58,8 +57,8 @@ void isPalindrome4(const std::string& str) {
 }
 
 void isPalindrome5(const std::string& str) {
-    std::string strcopy = str;
     std::cout << "isPalindrome5:\n";
+    std::string strcopy = str;
     for (auto it1 = strcopy.begin(), it2 = std::prev(strcopy.end()); it1 != strcopy.end(); ++it1, --it2) {
         if ((it1 - strcopy.begin()) < str.size() / 2) {
             std::iter_swap(it1, it2);
@@ -74,8 +73,8 @@ void isPalindrome5(const std::string& str) {
 }
 
 void isPalindrome6(const std::string& str) {
-    std::string strcopy;
     std::cout << "isPalindrome6:\n";
+    std::string strcopy;
     std::copy(str.crbegin(), str.crend(), std::back_inserter(strcopy));
     if (strcopy == str) {
         std::cout << str << " is palindrome" << '\n';
@@ -86,8 +85,8 @@ void isPalindrome6(const std::string& str) {
 }
 
 void isPalindrome7(const std::string& str) {
-    std::string strcopy;
     std::cout << "isPalindrome7:\n";
+    std::string strcopy;
     std::copy_if(str.crbegin(), str.crend(), std::back_inserter(strcopy), [](char c) { return true; });
     if (strcopy == str) {
         std::cout << str << " is palindrome" << '\n';
@@ -98,9 +97,79 @@ void isPalindrome7(const std::string& str) {
 }
 
 void isPalindrome8(const std::string& str) {
-    std::string strcopy;
     std::cout << "isPalindrome8:\n";
+    std::string strcopy;
     std::copy_n(str.crbegin(), str.size(), std::back_inserter(strcopy));
+    if (strcopy == str) {
+        std::cout << str << " is palindrome" << '\n';
+    } else {
+        std::cout << str << " is not palindrome" << '\n';
+    }
+    std::cout << '\n';
+}
+
+void isPalindrome9(const std::string& str) {
+    std::cout << "isPalindrome9:\n";
+    std::string strcopy;
+    strcopy.resize(str.size());
+    std::copy_backward(str.crbegin(), str.crend(), strcopy.end());
+    if (strcopy == str) {
+        std::cout << str << " is palindrome" << '\n';
+    } else {
+        std::cout << str << " is not palindrome" << '\n';
+    }
+    std::cout << '\n';
+}
+
+void isPalindrome10(const std::string& str) {
+    std::cout << "isPalindrome10:\n";
+    std::string strcopy;
+    bool not_palindrome;
+    int length = str.size();
+    for (int i = 0; i < length; i++) {
+        if (str[i] != str[length - i - 1]) {
+            not_palindrome = true;
+            break;
+        }
+    }
+    if (!not_palindrome) {
+        std::cout << str << " is palindrome" << '\n';
+    } else {
+        std::cout << str << " is not palindrome" << '\n';
+    }
+    std::cout << '\n';
+}
+
+void isPalindrome11(const std::string& str) {
+    std::cout << "isPalindrome11:\n";
+    std::string strcopy;
+    std::move(str.crbegin(), str.crend(), std::back_inserter(strcopy));
+    if (strcopy == str) {
+        std::cout << str << " is palindrome" << '\n';
+    } else {
+        std::cout << str << " is not palindrome" << '\n';
+    }
+    std::cout << '\n';
+}
+
+void isPalindrome12(const std::string& str) {
+    std::cout << "isPalindrome12:\n";
+    std::string strcopy;
+    strcopy.resize(str.size());
+    std::move_backward(str.crbegin(), str.crend(), strcopy.end());
+    if (strcopy == str) {
+        std::cout << str << " is palindrome" << '\n';
+    } else {
+        std::cout << str << " is not palindrome" << '\n';
+    }
+    std::cout << '\n';
+}
+
+void isPalindrome13(const std::string& str) {
+    std::cout << "isPalindrome13:\n";
+    std::string strcopy;
+    strcopy.resize(str.size());
+    std::transform(str.crbegin(), str.crend(), strcopy.begin(), [](char c) { return c; });
     if (strcopy == str) {
         std::cout << str << " is palindrome" << '\n';
     } else {
@@ -126,5 +195,15 @@ int main() {
     isPalindrome7(not_palindrom);
     isPalindrome8(palindrom);
     isPalindrome8(not_palindrom);
+    isPalindrome9(palindrom);
+    isPalindrome9(not_palindrom);
+    isPalindrome10(palindrom);
+    isPalindrome10(not_palindrom);
+    isPalindrome11(palindrom);
+    isPalindrome11(not_palindrom);
+    isPalindrome12(palindrom);
+    isPalindrome12(not_palindrom);
+    isPalindrome13(palindrom);
+    isPalindrome13(not_palindrom);
     return 0;
 }
